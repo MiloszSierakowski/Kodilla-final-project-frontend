5c3177467c56179a6f8de062cbbd1820bdb44506
Miłosz Sierakowski <roland_m1@wp.pl>
2023-03-30 23:46:24 +0200

    0.0 first commit

---
+++ src/main/java/com/kodilla/book/KodillaFinalProjectFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaFinalProjectFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaFinalProjectFrontendApplication.class, args);
+    }
+
+}
